c34dcb07d33aff332d5ec38c0297d16711ad4369
Kevin Ng <kevinwng23@gmail.com>
2023-09-04 16:19:15 -0400

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
 export default function Navbar() {
-    return (
-        <div className="nav">
-            <a href="/">Lorem Ipsum</a>
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                    <a href="/about">About Me</a>
-                    <a href="/work">Work</a>
-                    <a href="/contact">Contact</a>
-                    <a href="/resume">Resume</a>
-                </li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="nav">
+      <a href="/">Lorem Ipsum</a>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About Me</a>
+        </li>
+        <li>
+          <a href="/work">Work</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+        <li>
+          <a href="/resume">Resume</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
