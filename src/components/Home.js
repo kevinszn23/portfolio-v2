602bed3d6e30dd74427869c0db474dddac85fed7
Kevin Ng <kevinwng23@gmail.com>
2023-09-05 16:07:47 -0400

    added placeholder socials and refactored code with hero containers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,24 @@
 export default function Home() {
   return (
-    <div className="hero_container">
-      <div className="name_container">
-        <h1 className="name">Kevin Ng</h1>
+    <div className="hero-container">
+      <div className="hero-left-container">
+        <div className="hero-name-container">
+          <h1 className="hero-name">Kevin Ng</h1>
+        </div>
+        <div className="hero-socials-container">
+          <ul>
+            <li>
+              <a href="https://github.com/kevinszn23">github</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/kevinng23/">linkedin</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="location_container">
-        <h2 className="location">Software Engineer, based in Massachusetts.</h2>
-        <p className="aboutme">
+      <div className="hero-right-container">
+        <h2 className="hero-location">Software Engineer, based in Massachusetts.</h2>
+        <p className="hero-aboutme">
           I'm a former healthcare professional turned software engineer with
           knowledge in various tech-stacks like React, TypeScript, Python, and
           more. When I'm not coding, I'm most likely watching NBA games!
