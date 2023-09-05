fe53016c320b4dcf13baf66a19b4621341dbf416
Kevin Ng <kevinwng23@gmail.com>
2023-09-05 15:07:32 -0400

    styling home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,22 @@
 export default function Home() {
     return (
-        <div className="name">
-            Kevin Ng
-            <div className="aboutme">
-                <p>
-                    I'm a former healthcare professional turned software engineer with knowledge in various tech-stacks like React, TypeScript, Python, and more.
-                    <br />
+        <div className="name_container">
+            <h1 className="name">
+            Kevin 
+            Ng
+            </h1>
+            <div>
+                <div className="location_container">
+                    <h2 className="location">
+                        Software Engineer,
+                        based in Massachusetts.
+                    </h2>
+                    <p className="aboutme">
+                    I'm a former healthcare professional turned software engineer
+                    with knowledge in various tech-stacks like React, TypeScript, Python, and more.
                     When I'm not coding, I'm most likely watching NBA games!
-                </p>
+                    </p>
+                </div>
             </div>
         </div>
     )
