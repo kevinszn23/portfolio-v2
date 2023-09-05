f2cbd9e5537635c25099a5855cd8c0e23bce4fa6
Kevin Ng <kevinwng23@gmail.com>
2023-09-05 15:53:26 -0400

    added hero container and fixed alignments

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,17 @@
 export default function Home() {
-    return (
-        <div className="name_container">
-            <h1 className="name">
-            Kevin 
-            Ng
-            </h1>
-                <div className="location_container">
-                    <h2 className="location">
-                        Software Engineer,
-                        based in Massachusetts.
-                    </h2>
-                    <p className="aboutme">
-                    I'm a former healthcare professional turned software engineer
-                    with knowledge in various tech-stacks like React, TypeScript, Python, and more.
-                    When I'm not coding, I'm most likely watching NBA games!
-                    </p>
-                </div>
-            </div>
-    )
+  return (
+    <div className="hero_container">
+      <div className="name_container">
+        <h1 className="name">Kevin Ng</h1>
+      </div>
+      <div className="location_container">
+        <h2 className="location">Software Engineer, based in Massachusetts.</h2>
+        <p className="aboutme">
+          I'm a former healthcare professional turned software engineer with
+          knowledge in various tech-stacks like React, TypeScript, Python, and
+          more. When I'm not coding, I'm most likely watching NBA games!
+        </p>
+      </div>
+    </div>
+  );
 }
