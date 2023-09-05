2c575f837a02fc3cef8ba2b35324b174ebac2fac
Kevin Ng <kevinwng23@gmail.com>
2023-09-05 14:17:12 -0400

    about me added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ export default function Home() {
         <div className="name">
             Kevin Ng
             <div className="aboutme">
-                
+                <p>
+                    I'm a former healthcare professional turned software engineer with knowledge in various tech-stacks like React, TypeScript, Python, and more.
+                    <br />
+                    When I'm not coding, I'm most likely watching NBA games!
+                </p>
             </div>
         </div>
     )
