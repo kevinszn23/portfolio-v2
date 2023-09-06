d9199e976a23603229fa29678677c6680d9c19d4
Kevin Ng <kevinwng23@gmail.com>
2023-09-06 14:13:23 -0400

    added react countdown timer and streamify

--- src/components/Work.js
+++ src/components/Work.js
@@ -10,6 +10,24 @@ export default function Work() {
           <img className="projects-card-image" src="./images/original-gs-contracting.png" alt="Original G.S Contracting" />
         </a>
       </div>
+      <div className="projects">
+        <a
+          href="https://reactcountdowntimer.netlify.app/"
+          target="_blank"
+          rel=" noopener noreferrer"
+        >
+          <img className="projects-card-image" src="./images/react-countdown-timer.png" alt="React Countdown Timer" />
+        </a>
+      </div>
+      <div className="projects">
+        <a
+          href="https://streamify.fly.dev/"
+          target="_blank"
+          rel=" noopener noreferrer"
+        >
+          <img className="projects-card-image" src="./images/streamify.png" alt="Streamify" />
+        </a>
+      </div>
     </div>
   );
 }
