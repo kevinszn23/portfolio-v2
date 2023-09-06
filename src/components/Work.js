1933d2c5e520ba66a580e8620a3af0c1b4d21041
Kevin Ng <kevinwng23@gmail.com>
2023-09-06 13:22:07 -0400

    OGS project added

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,15 @@
 export default function Work() {
-    return (
-        <div className="projects">
-            <a href="https://original-gs-contracting.fly.dev/" target="_blank" rel=" noopener noreferrer">
-                <img src="./images/adsfhasdfhaksj.jpg" alt="" />
-            </a>
-        </div>
-    )
+  return (
+    <div className="projects-container">
+      <div className="projects">
+        <a
+          href="https://original-gs-contracting.fly.dev/"
+          target="_blank"
+          rel=" noopener noreferrer"
+        >
+          <img className="projects-card-image" src="./images/original-gs-contracting.png" alt="Original G.S Contracting" />
+        </a>
+      </div>
+    </div>
+  );
 }
