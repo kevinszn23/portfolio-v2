fbaa2098919b650185091ff8b3a6b9438a43c633
Kevin Ng <kevinwng23@gmail.com>
2023-09-06 14:28:41 -0400

    added project titles and descriptions

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,6 +9,14 @@ export default function Work() {
         >
           <img className="projects-card-image" src="./images/original-gs-contracting.png" alt="Original G.S Contracting" />
         </a>
+        <div className="projects-info">
+            <h1>
+                Original G.S Contracting
+            </h1>
+            <p>
+                Full-stack Django/Python website made for a contracting company. Features a contact page that stores user data and implemented all designing such as the logo with input from the company.
+            </p>
+        </div>
       </div>
       <div className="projects">
         <a
@@ -18,6 +26,12 @@ export default function Work() {
         >
           <img className="projects-card-image" src="./images/react-countdown-timer.png" alt="React Countdown Timer" />
         </a>
+        <h1>
+            React Countdown Timer
+        </h1>
+        <p>
+            A React timer that utilizes React hooks where one can put in a name, date, and optional time.
+        </p>
       </div>
       <div className="projects">
         <a
@@ -27,6 +41,12 @@ export default function Work() {
         >
           <img className="projects-card-image" src="./images/streamify.png" alt="Streamify" />
         </a>
+        <h1>
+            Streamify
+        </h1>
+        <p>
+            Spotify clone that utilizes Spotify API so one can search for songs, albums, and artists just like the real Spotify.
+        </p>
       </div>
     </div>
   );
