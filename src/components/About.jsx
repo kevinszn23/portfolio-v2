c57f3689d798f20353deaa4931cc6d22c02d3cc5
Kevin Ng <kevinwng23@gmail.com>
2024-02-13 15:44:44 -0500

    added an aboutme on main/about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,21 @@
 export default function About() {
     return (
-        <div className="portrait">
+        <div>
+            <div className="portrait">
             <img src="https://picsum.photos/id/237/200/300" alt="portrait" />
+            </div>
+
+            <h2 className="skillset">
+            {/* place icons of techstack i know here */}
+            {/* maybe change this to a span? */}
+            </h2>
+
+            <p className="aboutme">
+                I'm Kevin, a Massachusetts-based software engineer with a background in healthcare. I pivoted into the technology space in hopes of combining my autonomous and collaborative skills from healthcare to developing real world applications.
+                {/* update this later */}
+            </p>
+            
+
         </div>
     )
 }
