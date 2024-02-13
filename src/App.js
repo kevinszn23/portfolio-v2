7e18ad8c89e0c877492840111e240d1405ac77bf
Kevin Ng <kevinwng23@gmail.com>
2024-02-13 15:24:32 -0500

    added placeholder img for self portrait

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import About from './components/About';
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <header className="App-header">
         <Navbar />
       <h1 className="name">KEVIN NG</h1>
+        <About/>
       </header>
     </div>
   );
---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <div className="portrait">
+            <img src="https://picsum.photos/id/237/200/300" alt="portrait" />
+        </div>
+    )
+}
